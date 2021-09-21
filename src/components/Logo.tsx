e1d7be584ed2d83c150dabf6c9814ce1df09ee95
Dakotah Godfrey <dakotahgodfrey@Dakotahs-MacBook-Air.local>
2021-09-21 19:19:32 -0400

    dark mode styling remove unused files

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import SpaceLogo from '../images/space-logo.svg';
+import LightLogo from '../images/logo-light.svg';
+import { useAppSelector } from '../app';
+import { selectSettings } from '../app/slices/settings';
 const Logo = () => {
-  return <img src={SpaceLogo} alt='' />;
+  const settingsSlice = useAppSelector(selectSettings);
+  const { darkModeSetting } = settingsSlice;
+  return <img src={darkModeSetting ? LightLogo : SpaceLogo} alt='' />;
 };
 
 export default Logo;
--- src/pages/helpers.ts
+++
@@ -1,3 +0,0 @@
-export const findLastDay = (y: number, m: number) => {
-  return new Date(y, m + 1, 0).toISOString().split('T')[0];
-};
