7f7d7d6a1492d5ba4d3194286c82948bde6a4bbc
Dakotah Godfrey <dakotahgodfrey@Dakotahs-MacBook-Air.local>
2021-09-21 14:41:47 -0400

    update midlleware for redux persist, add redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,35 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import postReducer from './slices/posts';
-export const store = configureStore({
-  reducer: { posts: postReducer },
-});
+import storage from 'redux-persist/lib/storage';
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedPostReducer = persistReducer(persistConfig, postReducer);
+
+export const store = configureStore({
+  reducer: { posts: persistedPostReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export let persistor = persistStore(store);
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
