ebc4a0664e44698fca49a66cba8d42c6413fd2f3
Dakotah Godfrey <dakotahgodfrey@Dakotahs-MacBook-Air.local>
2021-09-21 22:39:28 -0400

    add toast notification

--- src/components/ReactionBar.tsx
+++ src/components/ReactionBar.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import { IoShareSocialSharp } from 'react-icons/io5';
+
 interface ReactionBarProps {
   liked: boolean;
   date: string;
@@ -11,6 +12,15 @@ const ReactionBar: React.FC<ReactionBarProps> = ({
   handleLike,
   date,
 }) => {
+  const [showNotification, setShowNotification] = useState<boolean>(false);
+  const handleShare = () => {
+    setShowNotification(true);
+    navigator.clipboard.writeText(`${window.location.href}post/${date}`);
+    setTimeout(() => {
+      setShowNotification(false);
+      console.log('timeout');
+    }, 1000);
+  };
   return (
     <div className='reaction-bar'>
       <button className='btn-icon btn-heart' onClick={() => handleLike()}>
@@ -22,14 +32,12 @@ const ReactionBar: React.FC<ReactionBarProps> = ({
           <AiOutlineHeart />
         )}
       </button>
-      <button
-        className='btn-icon btn-share'
-        onClick={() =>
-          navigator.clipboard.writeText(`${window.location.href}post/${date}`)
-        }
-      >
+      <button className='btn-icon btn-share' onClick={() => handleShare()}>
         <IoShareSocialSharp />
       </button>
+      {showNotification && (
+        <div className='notification'>Link Copied to Clipboard</div>
+      )}
     </div>
   );
 };
